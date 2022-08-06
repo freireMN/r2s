a55f2bbb83d69291acf8c691c68b2b52a0387863
FreireMN <freireMN.Research@gmail.com>
2022-08-06 13:11:51 -0300

    Dataset creation tested

--- test/dataset_preparation.py
+++ test/dataset_preparation.py
@@ -1,23 +1,24 @@
 import sys, time
 from argparse import Namespace
 
+R2S_BASE_FOLDER = '/home/mnf/projects/phd/r2s/'
+R2S_UTIL = R2S_BASE_FOLDER + 'src/util/'
 
-hardpath = '/home/vagrant/repo/r2s/'
-sys.path.append(hardpath + 'src/util/')
+R2S_DATASET_FOLDER_IN  = R2S_BASE_FOLDER + 'data/ml-latest-small'
+R2S_DATASET_FOLDER_OUT = R2S_BASE_FOLDER + 'data/R2SDatasets/movielens-small' 
+R2S_IMPORT_DATASET_MESSAGE = 'Time elapsed {} to run {} algorithm '
+
+sys.path.append(R2S_UTIL)
+
 
 import dataset_import as r2sdata
 
-args = Namespace(folderIn='/source/datasets/movielens/ml-latest-small'
-                ,folderOut=hardpath + 'data/R2SDatasets/movielens-small'
-                ,extension='csv')
 
+args = Namespace(folderIn=R2S_DATASET_FOLDER_IN, folderOut=R2S_DATASET_FOLDER_OUT,extension='csv')
 
 ts = time.time()
 ret = r2sdata.import_dataset(args)
-
 te = time.time()
 
-
-
-print ('Time elapsed {} to run {} algorithm '.format(te-ts, ret) )
+print(R2S_IMPORT_DATASET_MESSAGE.format(te-ts, ret))
     
